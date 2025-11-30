6f785e674a5909437ef20a962777b53526df4035
Dang3r0us <49214993+Dang3r0us@users.noreply.github.com>
2025-11-29 22:04:35 -0500

    Modify makeWord to handle spaces in input
    Added handling for spaces in the makeWord function as well as fixed indentation

--- makeWord.m
+++ makeWord.m
@@ -1,14 +1,18 @@
 function [spriteList] = makeWord(word)
-CAPITALS = ones(1, 26);
-for i = 1:2
-    for j = 1:13
-        CAPITALS((i - 1) * 13 + j) = toIndex(j + 19, i + 30);
+    CAPITALS = ones(1, 26);
+    for i = 1:2
+        for j = 1:13
+            CAPITALS((i - 1) * 13 + j) = toIndex(j + 19, i + 30);
+        end
     end
-end
-%disp(CAPITALS);
-word = upper(word);
-spriteList = ones(size(word));
-for c = 1:length(word)
-    spriteList(c) = CAPITALS(double(word(c)) - 64);
-end
+
+    word = upper(word);
+    spriteList = ones(size(word));
+    for c = 1:length(word)
+        if word(c) == " "
+            spriteList(c) = 1;
+        else
+            spriteList(c) = CAPITALS(double(word(c)) - 64);
+        end
+    end
 end
