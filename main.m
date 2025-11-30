3b78b5b52b53f64a9cc263340a4126dc95d8c44e
Dang3r0us <49214993+Dang3r0us@users.noreply.github.com>
2025-11-29 22:16:18 -0500

    Testing loadBackground/create start screen

--- main.m
+++ main.m
@@ -1,3 +1,7 @@
-game = simpleGameEngine("retro_pack.png", 16, 16, 5);
-
-drawScene(game, makeWord('helloworld'));
+game = simpleGameEngine("retro_pack.png", 16, 16, 1);
+background = loadBackground(1, 19, 9);
+background(4, 5:13) = makeWord('game name');
+background(7, 4:16) = makeWord('press any key');
+drawScene(game, background);
+getKeyboardInput(game);
+fprintf("start game\n");
